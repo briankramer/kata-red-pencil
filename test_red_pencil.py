aca33c7bb5525efefe1503c7cf72616a60acff25
Brian Kramer <bkramer@tutorgen.com>
2019-03-28 17:50:11 -0400

    Red pencil if between 5 and 30% decrease

--- test_red_pencil.py
+++ test_red_pencil.py
@@ -14,3 +14,6 @@ class TestRedPencil(unittest.TestCase):
 
     def test_when_is_red_pencil_passed_31_percent_reduction_return_false(self):
         self.assertFalse(rp.is_red_pencil((dt(1, 1, 1), 1.00), (dt(1, 2, 1), .69)))
+
+    def test_when_is_red_pencil_passed_5_percent_reduction_return_true(self):
+        self.assertTrue(rp.is_red_pencil((dt(1, 1, 1), 1.00), (dt(1, 2, 1), .95)))
