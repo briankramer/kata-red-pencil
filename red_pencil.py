d241423763a1269ebe679cbaa5a2f39d57e21afc
Brian Kramer <bkramer@tutorgen.com>
2019-03-28 18:31:59 -0400

    When red pencil, stop if new price increase

--- red_pencil.py
+++ red_pencil.py
@@ -1,4 +1,5 @@
 def is_red_pencil(last, current, last_red_pencil=None):
+    '''Determines if a new red pencil is starting.'''
     if not isinstance(last, tuple) or not isinstance(current, tuple):
         print('is_red_pencil needs two tuples.')
         return None
@@ -10,3 +11,9 @@ def is_red_pencil(last, current, last_red_pencil=None):
     if last_red_pencil and (current[0] - last_red_pencil).days < 30:
         return False
     return True
+
+def should_red_pencil_end(last_red_pencil, current):
+    '''Determines if red pencil should end prematurely.'''
+    if current[1] > last_red_pencil[1]:
+        return True
+    return False
--- test_red_pencil.py
+++ test_red_pencil.py
@@ -2,7 +2,7 @@ import unittest
 from datetime import datetime as dt
 import red_pencil as rp
 
-class TestRedPencil(unittest.TestCase):
+class TestIsRedPencil(unittest.TestCase):
     def test_when_is_red_pencil_passed_arrays_return_none(self):
         self.assertIsNone(rp.is_red_pencil([dt(1, 1, 1), 1.00], [dt(1, 1, 1), 1.00]))
 
@@ -23,3 +23,7 @@ class TestRedPencil(unittest.TestCase):
 
     def test_when_is_red_pencil_passed_29_after_red_pencil_return_false(self):
         self.assertFalse(rp.is_red_pencil((dt(1, 1, 1), 1.00), (dt(1, 2, 1), .95), dt(1, 1, 3)))
+
+class TestShouldRedPencilEnd(unittest.TestCase):
+    def test_when_should_red_pencil_end_passed_new_greater_return_true(self):
+        self.assertTrue(rp.should_red_pencil_end((dt(1, 1, 1), 1.00), (dt(1, 2, 1), 1.01)))
