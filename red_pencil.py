8b6d43d43e1d212dacf04f895da38fd99e8b1754
Brian Kramer <bkramer@tutorgen.com>
2019-03-28 17:40:13 -0400

    If price doesn't go down, no red pencil

--- red_pencil.py
+++ red_pencil.py
@@ -1,2 +1,6 @@
 def is_red_pencil(last, current):
-    return None
+    if not isinstance(last, tuple) or not isinstance(current, tuple):
+        print('is_red_pencil needs two tuples.')
+        return None
+    if current[1] >= last[1]:
+        return False
--- test_red_pencil.py
+++ test_red_pencil.py
@@ -5,3 +5,6 @@ import red_pencil as rp
 class TestRedPencil(unittest.TestCase):
     def test_when_is_red_pencil_passed_arrays_return_none(self):
         self.assertIsNone(rp.is_red_pencil([dt(1, 1, 1), 1.00], [dt(1, 1, 1), 1.00]))
+
+    def test_when_is_red_pencil_passed_greater_second_price_return_false(self):
+        self.assertFalse(rp.is_red_pencil((dt(1, 1, 1), 1.00), (dt(1, 2, 1), 2.00)))
